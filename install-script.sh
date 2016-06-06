a1331eccbd67fc8b7ab7388ab6b0adf573567299
Oliver <obsta2010@hotmail.com>
2016-06-07 04:24:14 +0800

    applications for fresh install

---
+++ install-script.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+runlevel=$(id -u)
+
+if [ $runlevel != "0" ]; then
+    echo "We need root!" >&2
+    exit 1
+fi
+     
+echo -e "Starting my post-OS installation script...\n"
+
+echo -e "\n#==> Installing Graphical applications..."
+# Graphical
+dnf -y install tmux thunderbird firefox thunar vlc xfce4-terminal
+
+echo -e "\n#==> Installing Non-Graphical applications..."
+# Non-Graphical
+dnf -y install vim git python php nmap figlet
+
+echo -e "\n#==> Installing PIP applications..."
+pip install pyradio 
+
+echo -e "\n#==> Installing Group packages..."
+# Groups
+dnf -y groupinstall "C Development Tools and Libraries" "Administration Tools" "Development Tools"
+
+echo -e "\n#==> All finished!"
