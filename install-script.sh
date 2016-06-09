d6a7d938c1304faca7697ab4d02c863a93734143
Oliver Buckler <oliver.buckler@gmail.com>
2016-06-09 16:37:11 +0800

    Update install-script.sh

--- install-script.sh
+++ install-script.sh
@@ -20,7 +20,7 @@ dnf -y install tmux thunderbird firefox thunar vlc xfce4-terminal libreoffice gi
 
 echo -e "\n#==> Installing Non-Graphical applications..."
 # Non-Graphical
-dnf -y install vim git python php nmap figlet mc
+dnf -y install vim git python php nmap figlet mc ncurses*
 
 echo -e "\n#==> Installing PIP applications..."
 pip install pyradio 
